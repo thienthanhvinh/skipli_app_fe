66bea97b5458dc37d643e92815a82b810416d1d8
ThanhVinh <nguyenthienthanhvinh@gmail.com>
2025-07-23 08:05:18 +0700

    [23-07] fix(edit.jsx): update user

--- src/pages/Admin/Edit.jsx
+++ src/pages/Admin/Edit.jsx
@@ -3,6 +3,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 
 const token = localStorage.getItem('token');
 const serverUrl = import.meta.env.VITE_SERVER_URL;
@@ -12,7 +13,7 @@ const Create = () => {
   const [data, setData] = useState({
     userName: '',
     email: '',
-    phoneNumber: '',
+    phone: '',
   });
 
   const { id } = useParams();
@@ -30,11 +31,11 @@ const Create = () => {
         .then((res) => {
           const result = res.data;
           const user = result.data;
-          console.log('Result', user);
+          // console.log('User', user);
           setData({
             userName: user.userName ?? '',
             email: user.email ?? '',
-            phoneNumber: user.phone,
+            phone: user.phone,
           });
         })
         .catch((error) => {
@@ -62,32 +63,40 @@ const Create = () => {
   const handleEdit = async (e) => {
     e.preventDefault();
     try {
-      await axios
-        .put(`${serverUrl}/edit/${id}`, data, {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((res) => {
-          const result = res.json();
-          console.log('update result', result);
-        })
-        .catch((error) => {
-          console.error(`Error: ${error.message}`);
-          setLoading(false);
-        })
-        .finally(setLoading(false));
+      const response = await axios.put(`${serverUrl}/user/${id}`, data, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const result = response.data;
+      console.log('Result', result);
+      toast.success(result?.message);
     } catch (error) {
       console.error(`Error: ${error.message}`);
+      toast.error('Failed to update user. Please check again');
     }
   };
 
   return (
-    <div className="flex flex-col items-center gap-4">
-      <h3 className="font-semibold text-xl">Edit User</h3>
-      {loading && <p className="text-sm text-gray-400">Updating...</p>}
-      <form onSubmit={handleEdit}>
+    <form onSubmit={handleEdit}>
+      <div className="flex flex-col items-center gap-4">
+        <ToastContainer
+          position="top-right" // Vị trí hiển thị
+          autoClose={1500} // Tự động đóng sau 3s
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <h3 className="font-semibold text-xl">Edit User</h3>
+        {loading && <p className="text-sm text-gray-400">Loading...</p>}
+
         <div className="flex flex-col gap-4 max-w-lg w-full">
           <Input
             type="text"
@@ -114,8 +123,8 @@ const Create = () => {
             isLabel
             label="Phone Number"
             placeholder="Enter employee phone number"
-            name="phoneNumber"
-            value={data?.phoneNumber}
+            name="phone"
+            value={data?.phone}
             className="focus:border-blue-400 transition-all duration-200 text-sm px-4 w-full"
             onChange={handleChange}
           />
@@ -124,12 +133,12 @@ const Create = () => {
             onClick={handleEdit}
             secondary
             className="px-3 py-2 button-success text-white mt-1"
-            type="button"
+            type="submit"
             text="Edit Employee"
           />
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };
 
